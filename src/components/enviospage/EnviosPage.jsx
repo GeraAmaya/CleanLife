aadcae70cdde7cef33134ee244ab2941a7a494ec
Gera Amaya <gerardoamaya.10@gmail.com>
2024-08-11 21:36:17 -0300

    envios ok

--- src/components/enviospage/EnviosPage.jsx
+++ src/components/enviospage/EnviosPage.jsx
@@ -1,14 +1,13 @@
 import { useState, useEffect } from 'react';
 import Swal from 'sweetalert2';
-import { getStock, updateProduct } from '../../helpers/firebase';
+import { getStock, addShipment, updateProduct } from '../../helpers/firebase';
 import styles from './EnviosPage.module.css';
 
 const objectives = ['Banco', 'Aeropuerto', 'Triunfo Seguros', 'Cruz del Sur', 'IAF', 'IERIC'];
 
 function EnviosPage() {
   const [products, setProducts] = useState([]);
-  const [selectedObjective, setSelectedObjective] = useState('');
-  const [selectedItems, setSelectedItems] = useState([]);
+  const [newShipment, setNewShipment] = useState({ objective: '', items: [] });
   const [selectedProduct, setSelectedProduct] = useState({ product: '', quantity: '' });
 
   useEffect(() => {
@@ -22,13 +21,16 @@ function EnviosPage() {
 
   const handleAddProduct = () => {
     if (selectedProduct.product && selectedProduct.quantity) {
-      setSelectedItems([...selectedItems, { ...selectedProduct, quantity: Number(selectedProduct.quantity) }]);
+      setNewShipment(prevState => ({
+        ...prevState,
+        items: [...prevState.items, { ...selectedProduct, quantity: Number(selectedProduct.quantity) }]
+      }));
       setSelectedProduct({ product: '', quantity: '' });
     }
   };
 
-  const handleSendItems = async () => {
-    if (selectedItems.length > 0) {
+  const handleAddShipment = async () => {
+    if (newShipment.objective && newShipment.items.length > 0) {
       const result = await Swal.fire({
         title: '¿Estás seguro de enviar estos insumos?',
         text: "No podrás deshacer esta acción.",
@@ -39,8 +41,10 @@ function EnviosPage() {
       });
 
       if (result.isConfirmed) {
+        await addShipment(newShipment);
+
         // Descontar productos del stock
-        for (const item of selectedItems) {
+        for (const item of newShipment.items) {
           const product = products.find(p => p.name === item.product);
           if (product) {
             const newStock = Number(product.quantity) - item.quantity;
@@ -49,12 +53,11 @@ function EnviosPage() {
         }
 
         fetchStock(); // Actualizar stock después de envío
-
         Swal.fire('Envío registrado', 'Los insumos han sido descontados del stock.', 'success');
-        setSelectedItems([]); // Limpiar la lista de insumos seleccionados
+        setNewShipment({ objective: '', items: [] });
       }
     } else {
-      Swal.fire('Lista vacía', 'No has seleccionado ningún insumo.', 'info');
+      Swal.fire('Formulario incompleto', 'Debes seleccionar un objetivo y agregar al menos un producto.', 'warning');
     }
   };
 
@@ -63,8 +66,8 @@ function EnviosPage() {
       <h1>Gestión de Envíos</h1>
       <div className={styles.form}>
         <select
-          value={selectedObjective}
-          onChange={(e) => setSelectedObjective(e.target.value)}
+          value={newShipment.objective}
+          onChange={(e) => setNewShipment({ ...newShipment, objective: e.target.value })}
         >
           <option value="">Seleccionar Objetivo</option>
           {objectives.map((objective, index) => (
@@ -91,10 +94,10 @@ function EnviosPage() {
           onChange={(e) => setSelectedProduct({ ...selectedProduct, quantity: e.target.value })}
         />
         <button onClick={handleAddProduct}>Agregar Producto</button>
-        <button onClick={handleSendItems}>Enviar Insumos</button>
+        <button onClick={handleAddShipment}>Registrar Envío</button>
       </div>
       <div className={styles.shipmentItems}>
-        {selectedItems.map((item, index) => (
+        {newShipment.items.map((item, index) => (
           <div key={index} className={styles.shipmentItem}>
             <span>{item.product}</span>
             <span>{item.quantity}</span>
--- src/components/navbar/NavBar.jsx
+++ src/components/navbar/NavBar.jsx
@@ -1,13 +1,33 @@
+import { useState } from 'react';
 import { Link } from 'react-router-dom';
 import styles from '../navbar/NavBar.module.css';
+import LOGO from '../../../img/logo.png'
 
 function Navbar() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
+
   return (
     <nav className={styles.navbar}>
-      <Link to="/">Inicio</Link>
-      <Link to="/stock">Stock</Link>
-      <Link to="/envios">Envíos</Link>
-      <Link to="/reportes">Reportes</Link>
+      <div className={styles.logo}>
+        <Link to="/">
+          <img src={LOGO} alt="Logo" />
+        </Link>
+      </div>
+      <div className={`${styles.links} ${isOpen ? styles.open : ''}`}>
+        <Link to="/" onClick={toggleMenu}>Inicio</Link>
+        <Link to="/stock" onClick={toggleMenu}>Stock</Link>
+        <Link to="/envios" onClick={toggleMenu}>Envíos</Link>
+        <Link to="/reportes" onClick={toggleMenu}>Reportes</Link>
+      </div>
+      <div className={styles.hamburger} onClick={toggleMenu}>
+        <div className={styles.line}></div>
+        <div className={styles.line}></div>
+        <div className={styles.line}></div>
+      </div>
     </nav>
   );
 }
